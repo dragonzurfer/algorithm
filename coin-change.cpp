40cc62313cc789801827f7bd3fcc5f81f4f40c39
dragonzurfer <dragonzurfer@gmail.com>
2017-03-10 04:19:00 +0530

    coin change dp

--- coin-change.cpp
+++ coin-change.cpp
@@ -21,21 +21,27 @@ int main()
 	FOR1(i,n-1)
 		cin>>c[i];
 	cin>>amt;
-	int dp[n+1][amt];
+	int dp[amt+1][n];
 	clr(dp);
 	FOR1(i,n-1)
 		dp[0][i]=1;
 
-	FOR2(i,1,n-1)
+	FOR2(i,1,amt)
 	{
-		FOR2(j,0,amt-1)
+		FOR2(j,0,n-1)
 		{
-			if(c[i]<=i)
+			if(c[j]<=i)
 			dp[i][j]+=(dp[i-c[j]][j]);
 			if(j>0)
 			dp[i][j]+=(dp[i][j-1]);
 		}
 	}
-	cout<<"Number of ways:"<<dp[n][amt-1];
+	/*FOR1(i,amt)
+	{
+		FOR1(j,n-1)
+		cout<<dp[i][j]<<" ";
+		cout<<endl;
+	}*/
+	cout<<"Number of ways:"<<dp[amt][n-1];
 	return 0;
 }
