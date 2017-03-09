415ca12faafbbb95cc058c179f92c8818b5df9f5
dragonzurfer <dragonzurfer@gmail.com>
2017-03-10 03:37:43 +0530

    Coin change dp

---
+++ coin-change.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mp make_pair
+#define pb push_back
+#define vll vector<ll>
+#define F first
+#define S second
+#define pll pair<ll,ll>
+#define FOR1(i,a) for(i=0;i<=a;i++)
+#define FOR2(i,a,b) for(i=a;i<=b;i++)
+#define endl '\n'
+#define clr(a) memset(a,0,sizeof(a))
+#define all(x) x.begin(),x.end()
+typedef long long ll;
+
+
+int main()
+{
+	int n,i,j,amt;cin>>n;
+	int c[n];
+	FOR1(i,n-1)
+		cin>>c[i];
+	cin>>amt;
+	int dp[n+1][amt];
+	clr(dp);
+	FOR1(i,n-1)
+		dp[0][i]=1;
+
+	FOR2(i,1,n-1)
+	{
+		FOR2(j,1,amt-1)
+		{
+			if(s[i]<=i)
+			dp[i][j]+=(dp[i-s[j]][j]);
+			if(j>0)
+			dp[i][j]+=(dp[i][j-1]);
+		}
+	}
+	cout<<"Number of ways:"<<dp[n][m-1];
+	return 0;
+}
